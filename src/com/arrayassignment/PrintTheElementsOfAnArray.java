2ccedadaa44092487e22e5ad26dc5c0f1c5dd43f
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-29 14:04:52 +0530

    print the elements of an array

---
+++ src/com/arrayassignment/PrintTheElementsOfAnArray.java
@@ -0,0 +1,18 @@
+package com.arrayassignment;
+
+public class PrintTheElementsOfAnArray {
+
+        public static void main(String[] args) {
+            int [] arr = new int [] {1,2,3,4,5};
+
+            System.out.println("element of given array");
+            for (int i = 0; i < arr.length;i++ )
+            {
+                System.out.print(arr[i] + " ");
+            }
+
+
+        }
+
+    }
+
